eebb65d18a1dbe7ecbf891a8013fb643598ea676
alex <uliza.fonar.apteka@gmail.com>
2020-03-16 22:36:15 +0700

    Updates

---
+++ precache-manifest.5edba4be4761fdba9020c42fff3db338.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6264689bccd93b92f538e67174388bba",
+    "url": "/alex-altay/country-cards-react/index.html"
+  },
+  {
+    "revision": "b3869373a6352225e8dc",
+    "url": "/alex-altay/country-cards-react/static/css/main.b1db8322.chunk.css"
+  },
+  {
+    "revision": "8d94dc2e1ebc9466175a",
+    "url": "/alex-altay/country-cards-react/static/js/2.863dfb9a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/alex-altay/country-cards-react/static/js/2.863dfb9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3869373a6352225e8dc",
+    "url": "/alex-altay/country-cards-react/static/js/main.a63ac3f7.chunk.js"
+  },
+  {
+    "revision": "2ee180da1804332d4c84",
+    "url": "/alex-altay/country-cards-react/static/js/runtime-main.66910c38.js"
+  },
+  {
+    "revision": "2927f8de4268bae8f11c4dfe3adc72b5",
+    "url": "/alex-altay/country-cards-react/static/media/edit.2927f8de.svg"
+  }
+]);
